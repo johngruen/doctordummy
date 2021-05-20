447c0093e56d730b305289ad70782b8037bb7e13
John Gruen <john.gruen@gmail.com>
2021-05-20 13:39:50 +0200

    add dot files

---
+++ .storybook/Globals.svelte
@@ -0,0 +1,15 @@
+<script>
+    import "../public/global.css";
+
+</script>
+
+<style>
+    :global(*, *:before, *:after) {
+        box-sizing: border-box;
+    }
+
+</style>
+
+<div>
+    <slot />
+</div>
---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+
+import Globals from './Globals.svelte'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const decorators = [() => Globals]
+
